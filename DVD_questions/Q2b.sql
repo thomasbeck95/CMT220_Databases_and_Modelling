645f01171e6ee9b17267e02f598a66e0d1b3ccec
Thomas Beck <thomasbeck95@gmail.com>
2025-03-26 10:26:34 +0000

    update to 2b

--- DVD_questions/Q2b.sql
+++ DVD_questions/Q2b.sql
@@ -1,5 +1,11 @@
-select first_name || ' ' || last_name, sum(amount)
+-- 2 - 2 Get the customer who has spent the highest amount on rentals.
+
+select * from customer;
+
+select first_name || ' ' || last_name, sum(amount) as total_spend
 from payment
 join customer using(customer_id)
 group by customer_id
+order by total_spend desc
+limit 1
 ;
